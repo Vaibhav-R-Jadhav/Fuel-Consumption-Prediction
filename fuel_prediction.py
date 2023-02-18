87a82648c670ee18aa169439589776e34b4d1a1a
Vaibhav-R-Jadhav <125502890+Vaibhav-R-Jadhav@users.noreply.github.com>
2023-02-18 23:06:09 +0530

    Add files via upload

---
+++ fuel_prediction.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+import pickle as pk
+import streamlit as st
+
+loaded_model = pk.load(
+    open(r"C:\Users\Vaibhav\OneDrive\Documents\masai\ML\Fuel_Project\trained_model_rf.sav"","rb"))
+scaled_data = pk.load(
+    open(r"C:\Users\Vaibhav\OneDrive\Documents\masai\ML\scaled_data.sav","rb"))
+
+
+def input_converter(inp):
+    vcl = ['Two-seater', 'Minicompact', 'Compact', 'Subcompact', 'Mid-size', 'Full-size', 'SUV: Small', 'SUV: Standard',
+           'Minivan', 'Station wagon: Small', 'Station wagon: Mid-size', 'Pickup truck: Small',
+           'Special purpose vehicle', 'Pickup truck: Standard']
+    trans = ['AV', 'AM', 'M', 'AS', 'A']
+    fuel = ["D", "E", "X", "Z"]
+    lst = []
+    for i in range(6):
+        if (type(inp[i]) == str):
+            if (inp[i] in vcl):
+                lst.append(vcl.index(inp[i]))
+            elif (inp[i] in trans):
+                lst.append(trans.index(inp[i]))
+            elif (inp[i] in fuel):
+                if (fuel.index(inp[i]) == 0):
+                    lst.extend([1, 0, 0, 0])
+                    break
+                elif (fuel.index(inp[i]) == 1):
+                    lst.extend([0, 1, 0, 0])
+                    break
+                elif (fuel.index(inp[i]) == 2):
+                    lst.extend([0, 0, 1, 0])
+                    break
+                elif (fuel.index(inp[i]) == 3):
+                    lst.extend([0, 0, 0, 1])
+        else:
+            lst.append(inp[i])
+
+    arr = np.asarray(lst)
+    arr = arr.reshape(1, -1)
+    arr = scaled_data.transform(arr)
+    prediction = loaded_model.predict(arr)
+
+    return (f"The Fuel Consumption L/100km is {round(prediction[0], 2)}")
+
+
+
+input_converter(["SUV: Small",2.0,4,"AS",5.0,"Z"])
+
+# def main():
+#     # giving a title
+#     st.title("Fuel Consumption Prediction WebApp")
+#
+#     # getting the input data from user
+#     result = 0
+#
+#     Vehicle_class = st.text_input("Enter Vehicle class")
+#     Engine_size = st.text_input("Enter Engine Size")
+#     Cylinders = st.text_input("Enter number of Cylinders")
+#     Transmission = st.text_input("Enter Transmission type without number of gears")
+#     Co2_Rating = st.text_input("Enter CO2 Rating")
+#     Fuel_type = st.text_input("Enter Fuel type (D, E, X, Z)")
+#
+#     # code for prediction
+#
+#     # creating a button for prediction
+#     if st.button("Predict"):
+#         result = input_converter([Vehicle_class,Engine_size,Cylinders,Transmission,Co2_Rating,Fuel_type])
+#
+#     st.success(result)
+#
+#
+# if __name__ == "__main__":
+#     main()
