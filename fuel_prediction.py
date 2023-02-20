83d31f905487b7a9685efc8f97ee818fc036a5a3
Vaibhav-R-Jadhav <125502890+Vaibhav-R-Jadhav@users.noreply.github.com>
2023-02-20 20:16:18 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -25,7 +25,7 @@ img = get_img_as_base64("image.jpg")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-image: url("https://unsplash.com/photos/_WB5QFl39WI");
 background-size: 300%;
 background-position: top left;
 background-repeat: no-repeat;
