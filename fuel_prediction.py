26e93a32d89f561d289e11e34b0917c5aaa91f3c
Vaibhav-R-Jadhav <125502890+Vaibhav-R-Jadhav@users.noreply.github.com>
2023-02-20 12:31:48 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -104,8 +104,8 @@ def main():
     vehicle = ['Two-seater','Minicompact','Compact','Subcompact','Mid-size','Full-size','SUV: Small','SUV: Standard','Minivan','Station wagon: Small','Station wagon: Mid-size','Pickup truck: Small','Special purpose vehicle','Pickup truck: Standard']
     transmission = ['AV', 'AM', 'M', 'AS', 'A']
     fuel = ["D", "E", "X", "Z"]
-    la= st.markdown("<p> style='color: red;'>Select the vehicle class</p>", unsafe_allow_html=True)
-    Vehicle_class = st.selectbox(label = la,options = vehicle)
+    st.markdown("<p> style='color: red;'>Select the vehicle class</p>", unsafe_allow_html=True)
+    Vehicle_class = st.selectbox(label = "",options = vehicle)
     Engine_size = st.number_input("Enter Engine Size (please enter value in this range[1-7])")
     Cylinders = st.number_input("Enter number of Cylinders (please enter value in this range[1-16]",min_value = 1, max_value = 16)
     Transmission = st.selectbox("Select the Transmission",transmission)
