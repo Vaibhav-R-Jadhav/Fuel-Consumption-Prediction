a70d84624fb32e19d3593c4f136726a3ad4e3307
Vaibhav-R-Jadhav <125502890+Vaibhav-R-Jadhav@users.noreply.github.com>
2023-02-20 14:02:37 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -108,14 +108,14 @@ def main():
 
     Vehicle_class = st.selectbox(label = "Enter Vehicle class",options = vehicle)
     
-    css = """
+    css = {
         <style>
             .stSelectbox [data-testid='stMarkdownContainer'] {
                 color: white;
             }
             
         </style>
-    """
+    }
 
     st.write(css, unsafe_allow_html=True)
     
