39937533e358a22930854328f860ac449cf83521
Vaibhav-R-Jadhav <125502890+Vaibhav-R-Jadhav@users.noreply.github.com>
2023-02-20 19:45:06 +0530

    Update fuel_prediction.py

--- fuel_prediction.py
+++ fuel_prediction.py
@@ -144,7 +144,7 @@ def main():
     # creating a button for prediction
     if st.button("Predict 🔍"):
         result = input_converter([Vehicle_class,Engine_size,Cylinders,Transmission,Co2_Rating,Fuel_type])
-        st.popup(result)
+        st.warning(result)
 
 #     st.success(result)
 
